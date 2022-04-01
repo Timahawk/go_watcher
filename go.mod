3ee630ccef23bc740d897ef5ad472b5995df53d8
Tim Wendel <you@example.com>
2022-04-01 12:26:33 +0200

    Eleminated Errors, Added pointless tests, Changed Logger, added Docu

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/shirou/gopsutil/v3 v3.22.2
+	github.com/sirupsen/logrus v1.8.1
 )
 
 require (
--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"encoding/binary"
 	"encoding/json"
 	"flag"
 	"html/template"
-	"log"
-	"math"
 	"net/http"
+	"os"
 	"time"
+
+	_ "net/http/pprof"
 
 	"github.com/gorilla/websocket"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/sirupsen/logrus"
 )
 
-var upgrader = websocket.Upgrader{} // use default options
+// use default options
+var upgrader = websocket.Upgrader{}
 
 const (
 	// Time allowed to write a message to the peer.
@@ -37,44 +39,67 @@ var (
 	// PollPeriodFlag
 	pollPeriod = flag.Duration("period", 1000*time.Millisecond, "Poll Period in Milliseconds.")
 
+	// Created Stats Struct.
 	Stats PC_stats
-	// Wie oft die Website geupdated wird.
-	//pollPeriod = time.Millisecond * 1000
+
+	logger = logrus.New()
 )
 
 type PC_stats struct {
-	CPU_Load float64 `json:"cpu_load"`
-	Mem_Load float64 `json:"mem_load"`
+	CPU_Load  float64   `json:"cpu_load"`
+	Mem_Load  float64   `json:"mem_load"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 func init() {
 	// Starte die Goroutines welche die Daten holen
 	go GetCPULoad(*pollPeriod)
 	go GetMemLoad(*pollPeriod)
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logger.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	logger.SetLevel(logrus.InfoLevel)
+	//
+	logger.SetFormatter(
+		&logrus.TextFormatter{TimestampFormat: "2006/01/02 - 15:04:05",
+			FullTimestamp: true})
 }
 
 func main() {
+
 	flag.Parse()
 
 	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/", home)
 
-	log.Println("Listening on X", *addr)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	logger.Info("Listening on ", *addr)
+	logger.Fatal(http.ListenAndServe(*addr, nil))
+
 }
 
+// GetMemLoad changes PC_Stats.Mem_Load each interval.
+//
+// This goroutine also writes the Timestamp to PC_Stats.
 func GetMemLoad(interval time.Duration) {
 	ticker := time.NewTicker(interval)
 
 	for {
 		select {
 		case <-ticker.C:
-			v, _ := mem.VirtualMemory()
+			v, err := mem.VirtualMemory()
+			if err != nil {
+				panic(err)
+			}
 			Stats.Mem_Load = v.UsedPercent
+			Stats.Timestamp = time.Now()
 		}
 	}
 }
 
+// GetCPUoad changes PC_StatsCPU_Load each interval.
 func GetCPULoad(interval time.Duration) {
 	ticker := time.NewTicker(interval)
 
@@ -90,20 +115,41 @@ func GetCPULoad(interval time.Duration) {
 	}
 }
 
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("Error while upgrading", err)
-		return
-	}
+// This funcion is needed to listen on the Closes from the Client side.
+//
+// If you don´t listen to those messages, the Programm will try to write,
+// on a dead connection and fail.
+func listen_function(c *websocket.Conn, message chan []byte, C_close chan bool) {
+	c.SetReadLimit(maxMessageSize)
+	c.SetReadDeadline(time.Now().Add(pongWait))
+	c.SetPongHandler(func(string) error { c.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 
-	ticker := time.NewTicker(pingPeriod)
-	poll := time.NewTicker(*pollPeriod)
-	defer func() {
-		ticker.Stop()
-		poll.Stop()
-		c.Close()
-	}()
+	for {
+		_, mess, err := c.ReadMessage()
+
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				logger.Info("IsUnexpectedCloseError", err)
+			}
+
+			logger.Info("Websocket Connection closed.")
+
+			message <- mess
+			close(message)
+			C_close <- true
+			close(C_close)
+
+			return
+		}
+	}
+}
+
+// write_function writes to the Connection, at specified interval.
+//
+// If the connection is closed on the client side, the goroutine is notified
+// via the C_close Channel and returns.
+// Additionally it regularly writes a PingMessage to the connection.
+func write_function(c *websocket.Conn, poll *time.Ticker, ticker *time.Ticker, C_close chan bool) {
 	for {
 		select {
 		case <-poll.C:
@@ -111,38 +157,68 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
 			w, err := c.NextWriter(websocket.TextMessage)
 			if err != nil {
-				log.Fatalln("c.NextWriter did not work", err)
+				logger.Warn("c.NextWriter did not work", err)
 			}
 			json, err := json.Marshal(Stats)
 			if err != nil {
-				log.Fatalln("Marshalling did not work", err)
+				logger.Fatalln("Marshalling did not work", err)
 			}
 
 			w.Write(json)
 
 			if err := w.Close(); err != nil {
-				log.Println("io.Writer Close did not work", err, r)
+				logger.Warn("io.Writer Close did not work", err)
 			}
 		case <-ticker.C:
 
 			c.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.WriteMessage(websocket.PingMessage, nil); err != nil {
-				log.Fatalln("c.WriteMessage did not work", err)
+				logger.Warn("c.WriteMessage did not work", err)
 			}
+		case <-C_close:
+			return
 		}
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	log.Println("Connection", r)
-	homeTemplate.Execute(w, nil)
+// Websocket Connection handler.
+//
+// TODO implement a way to stop the listen_function when write_functions gets an error.
+func echo(w http.ResponseWriter, r *http.Request) {
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		logger.Warn("Error while upgrading", err)
+		return
+	}
+
+	logger.Info("WebsocketConnection established by:", r.RemoteAddr)
+
+	ticker := time.NewTicker(pingPeriod)
+	poll := time.NewTicker(*pollPeriod)
+
+	message := make(chan []byte)
+
+	C_close := make(chan bool)
+
+	defer func() {
+		ticker.Stop()
+		poll.Stop()
+		c.Close()
+
+	}()
+
+	go listen_function(c, message, C_close)
+	go write_function(c, poll, ticker, C_close)
+
+	<-message
 }
 
-// https://stackoverflow.com/questions/43693360/convert-float64-to-byte-array
-func float64ToByte(f float64) []byte {
-	var buf [8]byte
-	binary.LittleEndian.PutUint64(buf[:], math.Float64bits(f))
-	return buf[:]
+func home(w http.ResponseWriter, r *http.Request) {
+
+	// Das hier läuft zweimail weil, /favicon.ico auch hierher geroutete wird.
+	logger.Info("Connection", r.RemoteAddr)
+	homeTemplate.Execute(w, nil)
 }
 
 var homeTemplate = template.Must(template.New("").Parse(`
@@ -150,6 +226,7 @@ var homeTemplate = template.Must(template.New("").Parse(`
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="icon" href="data:,">
 </head>
 <body>
 <div id="chart_div"></div>
